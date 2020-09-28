490e521224a4f3e83d5707292faf60da42963756
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 01:43:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,9 +16,14 @@ end
 
 def create_table
   sql = <<-SQL
-  CREATE TABLE students
+  CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  grade INTEGER
+  )
   SQL
   
+  DB[:conn].execute(sql)
 end
 
 end
