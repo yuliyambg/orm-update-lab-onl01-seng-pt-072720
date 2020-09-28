577a4d32d191b02947e45f834d1ee2f4f5191c61
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 01:39:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+attr_accessor :name, :grade
+attr_reader :id
 
+def initialize(name, grade, id=nil)
+@id = id
+@name = name
+@grade = grade
+end
 
 end
