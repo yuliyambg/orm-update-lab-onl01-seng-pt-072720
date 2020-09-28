3ad1116ce9d101946b9d9c5481df5bf5c3c980db
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 02:10:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,8 @@ end
 
 
 def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    new_student = self.new(row[0],row[1],row[2])
+
     new_student
   end
 
