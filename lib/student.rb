0d4fa938e3f2228c63236fe030ddaf725219bf5a
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 01:50:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,17 @@ def self.drop_table
   DB[:conn].execute(sql)
 end
 
-
+ def save
+   if self.id
+     self.update
+   else
+ sql = <<-SQL
+  INSERT INTO songs (name, album)
+  VALUES (?, ?)
+ SQL
+ DB[:conn].execute(sql, self.name, self.album)
+ @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+end
 
 
 end
