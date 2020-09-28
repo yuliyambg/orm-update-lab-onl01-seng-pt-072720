eb148fdceab8b7381fd1e9154c503508c9386179
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 01:55:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ end
 
 def update
 sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-DB[:conn].execute(sql, self.name, self.grade self.id)
+DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 
 end
