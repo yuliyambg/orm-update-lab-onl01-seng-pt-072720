a65de683b9706453ca8b45091274ab1bfb7b9dd3
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-28 02:13:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,14 @@ def self.new_from_db(row)
 end
 
 def self.find_by_name(name)
-  
+  sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
 end
 
 
